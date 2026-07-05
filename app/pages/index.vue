ff2cc143a79df725598ef36ce825d6da859f6183
Khaomi <46065349+Khaomi@users.noreply.github.com>
2026-07-06 00:57:33 +0700

    feat: add disclaimer

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <h1 class="text-3xl font-bold underline">
-        Hello world!
+        Work in progress!<br>
+        This will take a very long while, I am not a website designer and I am not a good coder.
     </h1>
 </template>
